4fa5bb4a26e9d921eb0562b907259a1699369e39
Shashwat Srivastava <shashwatsu27@gmail.com>
2023-07-12 16:33:47 +0530

    added 2  questions with solution
    Added solution for 1st pushing element at bottom.  and 2nd Reversing String using Stack.

--- Stacks.java
+++ Stacks.java
@@ -3,97 +3,97 @@ import java.util.*;
 
 
 public class Stacks{
-  // public static class Stack{                                           // Stacks using ArrayList
-    // static ArrayList<Integer> list = new ArrayList<>();                      
+  public static class Stack{                                           // Stacks using ArrayList
+    static ArrayList<Integer> list = new ArrayList<>();                      
     
-    // public static boolean isEmpty(){
-    //   return list.size() == 0;
+    public static boolean isEmpty(){
+      return list.size() == 0;
 
-    // }
+    }
 
-    // public static void push(int data){
-    //   list.add(data);
+    public static void push(int data){
+      list.add(data);
 
-    // }
+    }
     
-    // public static int pop(){
-    //   if (isEmpty()){
-    //     return -1;
-    //   }
-    //   int top = list.get(list.size()-1);
-    //   list.remove(list.size()-1);
-    //   return top ;
+    public static int pop(){
+      if (isEmpty()){
+        return -1;
+      }
+      int top = list.get(list.size()-1);
+      list.remove(list.size()-1);
+      return top ;
 
-    // }
+    }
     
-    // public static int peek(){
-    //   if (isEmpty()){
-    //     return -1;
-    //   }
-    //   return list.get(list.size()-1);
-    // }
-  // }
+    public static int peek(){
+      if (isEmpty()){
+        return -1;
+      }
+      return list.get(list.size()-1);
+    }
+  }
 
 
 
 
-  // static class Node{                         //Stack using Linkedlist 
-  //     int data;
-  //     Node next;
-  //     Node(int data){
-  //       this.data=data;
-  //       this.next= null;
-  //     }
-  //   }
-  // public static class Stack{
-  //   static Node head = null;
+  static class Node{                         //Stack using Linkedlist 
+      int data;
+      Node next;
+      Node(int data){
+        this.data=data;
+        this.next= null;
+      }
+    }
+  public static class Stack{
+    static Node head = null;
 
-  //   public static boolean isEmpty(){
-  //     return head == null;
+    public static boolean isEmpty(){
+      return head == null;
 
-  //   }
-  //   public static void push(int data){
-  //     Node newNode = new Node(data);
+    }
+    public static void push(int data){
+      Node newNode = new Node(data);
 
-  //     if(isEmpty()){
-  //       head = newNode;
-  //       return;
+      if(isEmpty()){
+        head = newNode;
+        return;
 
-  //     }
-  //     newNode.next = head;
-  //     head = newNode;
+      }
+      newNode.next = head;
+      head = newNode;
 
-  //   }
+    }
 
-  //   public static int pop(){
-  //     if (isEmpty())
-  //       return -1;
-  //     int top = head.data;
-  //     head=head.next;
-  //     return top; 
+    public static int pop(){
+      if (isEmpty())
+        return -1;
+      int top = head.data;
+      head=head.next;
+      return top; 
 
-  //   }
+    }
 
-  //   public static int peek(){
-  //     return head.data;
-  //   }
+    public static int peek(){
+      return head.data;
+    }
 
-  // } 
-
-
+  } 
 
 
-  // public static void main(String args[]){               //main function for arraylist and linkedlist
-  //   Stack s = new Stack();
-  //   s.push(1);
-  //   s.push(2);
-  //   s.push(3);
 
-  //   while(!s.isEmpty()){
-  //     System.out.println(s.peek());
-  //     s.pop();
-  //   }
-  // }
+
+  public static void main(String args[]){               //main function for arraylist and linkedlist
+    Stack s = new Stack();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    while(!s.isEmpty()){
+      System.out.println(s.peek());
+      s.pop();
+    }
+  }
 
 
 
@@ -114,6 +114,63 @@ public class Stacks{
 
 
 
+
+
+
+  public static void placeAtBottom(Stack<Integer> s , int data){                           //placing element at Bottom of stack
+
+    if (s.isEmpty()){
+      s.push(data);
+      return;
+    }
+    int top= s.pop();
+    placeAtBottom(s,data);
+    s.push(top);
+
+  }
+
+
+
+  public static void main(String args[]){
+    Stack<Integer> s = new Stack<>();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+
+    placeAtBottom(s , 4);
+    while(!s.isEmpty()){
+      System.out.println(s.pop());
+
+    }
+  }
+  
+
+
+  public static String reverseString(String str){                        //reversing the string using Stack
+    Stack<Character> s =new Stack<>();
+    int idx= 0;
+    while (idx < str.length()){
+      s.push(str.charAt(idx));
+      idx++;
+
+    }
+
+    StringBuilder result = new StringBuilder("");
+    while(!s.isEmpty()){
+      char curr = s.pop();
+      result.append(curr);
+
+    }
+    return result.toString();
+
+  }
+  public static void main(String args[]){
+    String str= "HelloWorld";
+    String output = reverseString(str);
+    System.out.println(output);
+
+  }
 
 
 
